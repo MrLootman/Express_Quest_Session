d7d55c9d5743480ac29da92797c229c3b25281a8
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2023-01-01 23:35:14 +0100

    Express quest about sessions

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// GET session-in & session-out
+
 router.get('/session-in', function(req, res) {
   req.session.maVariable = "Be bop a lula";
   console.log(req.session);
